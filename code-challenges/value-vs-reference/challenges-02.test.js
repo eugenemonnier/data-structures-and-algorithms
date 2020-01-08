da6faa108e682a84bb3e3ee93e9ab646b7a4f483
EugeneMonnier <eugene.monnier@gmail.com>
2020-01-07 17:34:19 -0800

    completed challenge 4

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -66,6 +66,7 @@ console.log(people[1].isAuthor) prints true
 
 const setStatusAsAuthor = (people) => {
   // Solution code here...
+  people.forEach(people => people.isAuthor = true);
 };
 
 /* ------------------------------------------------------------------------------------------------
