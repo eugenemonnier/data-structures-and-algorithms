ec2c2c74fbe48b4a5d6ab58ba58b76a61df34f17
eugenemonnier <eugene.monnier@gmail.com>
2020-01-20 23:09:03 -0800

    completed challenge 3

--- code-challenges/regular-expressions-part2/challenges-11.test.js
+++ code-challenges/regular-expressions-part2/challenges-11.test.js
@@ -57,6 +57,7 @@ Return either true or false.
 
 const validatePhoneNumber = (phoneNumber) => {
   // Solution code here...
+  return /^(\([\d]{3}\)[\s]?|([\d]{3})[\s-]?)[\d]{3}[\s-]?[\d]{4}$/.test(phoneNumber);
 };
 
 /* ------------------------------------------------------------------------------------------------
