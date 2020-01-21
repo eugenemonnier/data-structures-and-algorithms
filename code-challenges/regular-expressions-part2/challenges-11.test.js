2d037775e827342dd65b4672ef258e0ce95b1957
eugenemonnier <eugene.monnier@gmail.com>
2020-01-20 22:55:05 -0800

    completed challenge 2

--- code-challenges/regular-expressions-part2/challenges-11.test.js
+++ code-challenges/regular-expressions-part2/challenges-11.test.js
@@ -31,6 +31,7 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 
 const validateEmail = (email) => {
   // Solution code here...
+  return /^\w+([\.]?\w+)@(\w+)(\.((com)|(net)|(org)))$/.test(email);
 };
 
 /* ------------------------------------------------------------------------------------------------
