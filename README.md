c3754c9f7717ecad0076578b6fb8fcfd60fc4f92
Eugene Monnier <eugene.monnier@gmail.com>
2020-02-10 15:09:15 -0800

    cleaned up array-reverse.js code and updated README.md with solution

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # data-structures-and-algorithms
 
 ## Reverse an Array
-Is it worth it? Let me work it.
-I put my arr down, flip it and reverse it. 
-Ti esrever dna ti pilf, nwod rra ym tup i.
-- Missy Elliot
+Is it worth it? Let me work it. <br/>
+I put my arr down, flip it and reverse it. </br>
+Ti esrever dna ti pilf, nwod rra ym tup i.</br>
+\- Missy Elliot
 
 ### Challenge
 Write a function called reverseArray which takes an array as an argument. Without utilizing any of the built-in methods available to your language, return an array with elements in reversed order.
@@ -15,3 +15,6 @@ I used the forEach method to iterate through the inputed array, passing through 
 Alternatively, I also fed the array through a for loop that sets the starting index at the array length minus 1. Each iteration of the loop the index value will be reduced by 1. In the loop, I push to a new array the value of the original array at the current index.
 
 Both of these methods will console log the original array reversed. Both of these methods allows arrays of any length to be reversed using 6 lines of code.
+
+### Solution
+![Array Reverse](./images/arrayReverse.jpg)
--- code-challenges/arrayreverse/array-reverse.js
+++ code-challenges/arrayreverse/array-reverse.js
@@ -1,7 +1,4 @@
 'use strict'
-const firstArr = [1, 2, 3, 4, 5, 6];
-const secondArr = [89, 2354, 3546, 23, 10, -923, 823, -12];
-const thirdArr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199];
 
 // reverses array by iterating through the array and setting the index of that value to the array's length minus the current index minus one.
 const reverseArray = (array) => {
@@ -9,7 +6,7 @@ const reverseArray = (array) => {
   array.forEach((value, index) => {
     reversedArr[array.length - index - 1] = value;
   });
-  return console.log(`Reversed array: ${reversedArr}`);
+  return reversedArr;
 }
 
 const altReverseArray = (array) => {
@@ -17,13 +14,5 @@ const altReverseArray = (array) => {
   for( var i = array.length - 1; i > 0; i--) {
     reversedArr.push(array[i]);
   }
-  return console.log(`Alternate reversed array: ${reversedArr}`);
+  return reversedArr;
 }
-
-reverseArray(firstArr);
-reverseArray(secondArr);
-reverseArray(thirdArr);
-
-altReverseArray(firstArr);
-altReverseArray(secondArr);
-altReverseArray(thirdArr);
