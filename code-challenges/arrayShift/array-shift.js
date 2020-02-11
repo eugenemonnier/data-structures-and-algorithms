dd234de1f9c30caffcea5523e2b5113d6e5382ae
Eugene Monnier <eugene.monnier@gmail.com>
2020-02-11 14:45:14 -0800

    completed code challenge

---
+++ code-challenges/arrayShift/array-shift.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const insertShiftArray = (arr, val) => {
+  let newArr = [];
+  let addIdx = Math.ceil(arr.length / 2);
+  arr.length === 0 ? newArr.push(val) : iterateArr(arr, val, addIdx, newArr);
+  return newArr;
+}
+
+const iterateArr = (arr, val, idx, newArr) => {
+  arr.forEach((elem, i) => {
+    i !== idx ? newArr.push(elem) : (newArr.push(val), newArr.push(elem));
+  });
+}
