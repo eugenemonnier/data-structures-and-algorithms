75d4bfb470a7de2a7f398c9f066c78da8d8ea4f5
Eugene Monnier <eugene.monnier@gmail.com>
2020-02-24 22:46:55 -0800

    create PseudoQueue class, methods, and tests

---
+++ code-challenges/Data-Structures/queueWithStacks/queue-with-stacks.js
@@ -0,0 +1,38 @@
+const { Stack, Node } = require('../stacksAndQueues/stacks-and-queues')
+
+class PseudoQueue extends Stack {
+
+  constructor() {
+    super(top)
+  }
+
+  enqueue(value) {
+    let firstStack = new PseudoQueue()
+    let secondStack = new PseudoQueue()
+    if(!this.top) {
+      secondStack.push(value)
+    } else {
+      let currNode = this.top
+      while(currNode.next) {
+        firstStack.push(currNode.value)
+        currNode = currNode.next
+      }
+      firstStack.push(currNode.value)
+      firstStack.push(value)
+      let otherNode = firstStack.top
+      while(otherNode.next) {
+        secondStack.push(otherNode.value)
+        otherNode = otherNode.next
+      }
+      secondStack.push(otherNode.value)
+    }
+    this.top = secondStack.top
+    return this.top
+  }
+
+  dequeue() {
+    return this.pop()
+  }
+}
+
+module.exports = PseudoQueue
