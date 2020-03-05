85f6296b3018d66ddbb97380cd658e402adfb36e
Eugene Monnier <eugene.monnier@gmail.com>
2020-03-04 20:59:12 -0800

    created maximumBinary function

--- code-challenges/Data-Structures/tree/tree.js
+++ code-challenges/Data-Structures/tree/tree.js
@@ -68,7 +68,7 @@ class BinaryTree {
   breadthOrder () {
     const output = []
     const queue = []
-    if (!this.root) return
+    if (!this.root) return output
     else queue.push(this.root)
     while (queue.length > 0) {
       const current = queue[0]
@@ -79,11 +79,41 @@ class BinaryTree {
     }
     return output
   }
+
+  reverseLevelOrder () {
+    const output = []
+    const queue = []
+    if (!this.root) return output
+    else queue.push(this.root)
+    while (queue.length > 0) {
+      const current = queue.shift()
+      if (current.right) queue.push(current.right)
+      if (current.left) queue.push(current.left)
+      output.unshift(current.value)
+    }
+    return output
+  }
+
+  maximumBinary () {
+    let output = -Infinity
+    const traverse = (Node) => {
+      if (!Node) {
+        return
+      }
+      if (Node.value > output) output = Node.value
+      if (Node.left) traverse(Node.left)
+      if (Node.right) traverse(Node.right)
+    }
+    traverse(this.root)
+    return output
+  }
 }
 
 module.exports = { Node, BinaryTree }
 
 const testTree = new BinaryTree()
+console.log('maximumBinary', testTree.maximumBinary())
+
 testTree.root = new Node(10)
 testTree.root.left = new Node(9)
 testTree.root.left.left = new Node(8)
@@ -92,7 +122,22 @@ testTree.root.right = new Node(5)
 testTree.root.right.left = new Node(4)
 testTree.root.right.left.left = new Node(2)
 
-console.log('preorder ', testTree.preOrder())
-console.log('inorder ', testTree.inOrder())
-console.log('postorder ', testTree.postOrder())
-console.log('breadth ', testTree.breadthOrder())
+const testTree2 = new BinaryTree()
+console.log('reverseLevelOrder empty ', testTree2.reverseLevelOrder())
+
+testTree2.root = new Node(1)
+testTree2.root.left = new Node(2)
+testTree2.root.left.left = new Node(4)
+testTree2.root.left.right = new Node(5)
+testTree2.root.right = new Node(3)
+testTree2.root.right.left = new Node(6)
+testTree2.root.right.right = new Node(7)
+
+// console.log('preorder ', testTree.preOrder())
+// console.log('inorder ', testTree.inOrder())
+// console.log('postorder ', testTree.postOrder())
+// console.log('breadth ', testTree.breadthOrder())
+console.log('reverseLevelOrder ', testTree2.reverseLevelOrder())
+console.log('reverseLevelOrder ', testTree.reverseLevelOrder())
+console.log('maximumBinary', testTree.maximumBinary())
+console.log('maximumBinary', testTree2.maximumBinary())
