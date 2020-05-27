7592c7cfa4523d681406825c4d02b6ae6b307c1a
Eugene Monnier <eugene.monnier@gmail.com>
2020-05-26 21:27:47 -0700

    completed code challenge and updated README

--- code_challenges/data_structures/stacks_and_queues/stacks_and_queues.py
+++ code_challenges/data_structures/stacks_and_queues/stacks_and_queues.py
@@ -8,7 +8,7 @@ class Stack:
         self.top = None
 
     def peek(self):
-        return self.top
+        return self.top.value
 
     def push(self, val):
         new_node = Node(val)
@@ -20,12 +20,14 @@ class Stack:
         if not self.top:
             return None
         elif not self.top.next:
+            popped = self.top
             self.top = None
-            return self.top
+            return popped
         else:
+            popped = self.top.value
             self.top.value = self.top.next.value
             self.top.next = self.top.next.next
-            return this.top.value
+            return popped
 
     def isEmpty(self):
         return not self.top
@@ -83,3 +85,44 @@ class Queue:
             curr_node = curr_node.next
         queue += str(curr_node.value) + ' -> None'
         return queue
+
+class PseudoQueue(Stack):
+  def __init__(self):
+      super().__init__()
+      self.top = None
+
+  def enqueue(self, value):
+    first_stack = PseudoQueue()
+    second_stack = PseudoQueue()
+    if not self.top:
+        second_stack.push(value)
+    else:
+      curr_node = self.top
+      print(self)
+      while curr_node.next:
+        first_stack.push(curr_node.value)
+        curr_node = curr_node.next
+      first_stack.push(curr_node.value)
+      first_stack.push(value)
+      print(first_stack)
+      other_node = first_stack.top
+      while other_node.next:
+        second_stack.push(other_node.value)
+        other_node = other_node.next
+    second_stack.push(other_node.value)
+    self.top = second_stack.top
+    return self.top
+
+  def dequeue(self):
+    return self.pop()
+
+def __str__(self):
+    pass
+
+if __name__ == "__main__":
+  test_pseudo = PseudoQueue()
+  test_pseudo.push(1)
+  test_pseudo.push(2)
+  test_pseudo.push(3)
+  test_pseudo.enqueue(4)
+  print(test_pseudo)
